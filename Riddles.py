8c039b9e472e576e2bdeea2a81933b42821469fd
MEME30610 <afos30610@gmail.com>
2026-07-06 09:54:56 -0400

    Add files via upload

---
+++ Riddles.py
@@ -0,0 +1,18 @@
+print("What has to be broken before you can use it?")
+answer=input()
+if answer=="egg" or answer=="Egg":
+    print("correct")
+else:
+    print("incorrect")
+print()
+
+print("I have a mouth but never talk.")
+print("I have a bed but never sleep.")
+print("I always run but never walk.")
+print("I have a head but never weep.")
+print("What am I?")
+answer=input()
+if answer.lower()=="river":
+    print("correct")
+else:
+    print("incorrect")
