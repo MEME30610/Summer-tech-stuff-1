4ff44461ed22d615effb55c7560eed2d4db21553
MEME30610 <afos30610@gmail.com>
2026-07-06 10:45:28 -0400

    Add files via upload

---
+++ calculator2.py
@@ -0,0 +1,16 @@
+print("type a number")
+number1=float(input())
+print("type another number")
+number2=float(input())
+print("input an operation")
+operation=input()
+if operation=="+":
+    print(number1+number2)
+elif operation=="-":
+    print(number1-number2)
+elif operation=="*":
+    print(number1*number2)
+elif operation=="/":
+    print(number1/number2)
+elif operation=="%":
+    print(number1%number2)
